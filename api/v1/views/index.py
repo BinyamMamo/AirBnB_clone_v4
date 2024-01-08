94be59897e22c8e8234f0b79e887c9eaeaeedc2d
BinyamMamo <binyammamo01@gmail.com>
2024-01-08 20:19:51 +0400

    Mon Jan  8 20:19:51 +04 2024

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,11 +6,6 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    return jsonify({"status": "ok"})
-
-
-@app_views.route('/stats', methods=['GET'])
-def stats():
     return jsonify({"status": "ok"})

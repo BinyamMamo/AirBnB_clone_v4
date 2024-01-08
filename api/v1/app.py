37a1307c87508b9315bbe65f2a16363a1583af76
BinyamMamo <binyammamo01@gmail.com>
2024-01-08 19:35:47 +0400

    Mon Jan  8 19:35:47 +04 2024

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,13 +8,15 @@ and an error handler for 404 errors.
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views.index import app_views
+# from api.v1.views.index import app_views
 from os import getenv
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
-@app.teardown_appcontext()
+
+@app.teardown_appcontext
 def close_session(exception):
     """Closes the app session
     """
@@ -28,6 +30,7 @@ def not_found(err):
     return jsonify({'error': 'Not found'}), 404
 
 
-
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST"), port=getenv("HBNB_API_PORT"), threaded=True)
+    HBNB_API_HOST = getenv("HBNB_API_HOST")
+    HBNB_API_PORT = getenv("HBNB_API_PORT")
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ defines two routes for a Flask application.
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "ok"})

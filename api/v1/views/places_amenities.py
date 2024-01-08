d87b86000af7c5f657d1309990b4f33d9044698c
BinyamMamo <binyammamo01@gmail.com>
2024-01-09 00:25:12 +0400

    Tue Jan  9 00:25:12 +04 2024

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -5,9 +5,9 @@ Retrieve default API actions for Place and Amenity view objects link.
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, make_response, request
 from flask_cors import CORS
+from flasgger import Swagger
 from models import storage
 from os import getenv
-from flasgger import Swagger
 
 
 app = Flask(__name__)

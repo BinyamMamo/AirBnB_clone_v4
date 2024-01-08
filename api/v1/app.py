bd59da14ed2518a023608b0b7911c9c491e297c2
BinyamMamo <binyammamo01@gmail.com>
2024-01-09 01:09:32 +0400

    Tue Jan  9 01:09:32 +04 2024

--- api/v1/app.py
+++ api/v1/app.py
@@ -33,8 +33,6 @@ def not_found(err):
 
 
 if __name__ == "__main__":
-    HBNB_API_HOST = getenv("HBNB_API_HOST")
-    HBNB_API_PORT = getenv("HBNB_API_PORT")
-    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
-    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
+    host = '0.0.0.0' or getenv("HBNB_API_HOST")
+    port = 5000 or getenv("HBNB_API_PORT")
     app.run(host=host, port=port, threaded=True)
